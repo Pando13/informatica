91b172fbac8429906b6121e09a0a61f6b08273b4
Pando13 <meme04pando@gmail.com>
2022-12-09 13:13:28 +0100

    non ho capito i super

--- triangolo/TestTriangolo.java
+++ triangolo/TestTriangolo.java
@@ -5,7 +5,8 @@ public class TestTriangolo {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
 		Triangolo t1=null;
-		int scelta,p,lato1,lato2,lato3;
+		int scelta,p,lato;
+        String vuota;
 
         do{
             System.out.println("premi 0 per uscire");
@@ -15,12 +16,49 @@ public class TestTriangolo {
             System.out.println("premi 4 per calcolare l'altezza");
             System.out.println("premi 5 per calcolare il perimetro");
             System.out.println("premi 6 per calcolare l'area");
+            scelta=input.nextInt();
+			vuota=input.nextLine();
 
-            switch (key) {
-                case value:
+            switch (scelta) {
+                case 0:
+                    break;
+                
+                case 1:
+                    do{
+                        System.out.println("dammi lato");
+                        lato=input.nextInt();
+                        vuota=input.nextLine();
+						if(lato<1){
+							System.out.println("errore");
+						}
+					}while(lato<1);
+                    t1=new Triangolo(lato);
+                    break;
+                
+                case 2:
+                    if (t1!=null) {
+                        t1.visualizza();
+                    }else{
+                        System.out.println("errore");
+                    }
+                    break;
+
+                case 3:
                     
                     break;
-            
+                
+                case 4:
+                    
+                    break;
+                
+                case 5:
+                    
+                    break;
+                
+                case 6:
+                    
+                    break;
+                
                 default:
                     break;
             }
--- triangolo/Triangolo.java
+++ triangolo/Triangolo.java
@@ -1,5 +1,13 @@
 package triangolo;
 
 public class Triangolo {
-    
+    int lato;
+
+	public Triangolo(int lato){
+		this.lato=lato;
+	}
+
+	void visualizza(){
+		System.out.println("lato = "+this.lato);
+	}
 }
