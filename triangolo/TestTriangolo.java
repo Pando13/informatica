ac95e79d06028252c6e211df58149d791a433a64
Pando13 <meme04pando@gmail.com>
2022-12-09 12:55:10 +0100

    franz

---
+++ triangolo/TestTriangolo.java
@@ -0,0 +1,29 @@
+package triangolo;
+import java.util.*;
+
+public class TestTriangolo {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+		Triangolo t1=null;
+		int scelta,p,lato1,lato2,lato3;
+
+        do{
+            System.out.println("premi 0 per uscire");
+            System.out.println("premi 1 per creare un triangolo");
+            System.out.println("premi 2 per visualizzare i lati");
+            System.out.println("premi 3 per dire se e' un triangolo");
+            System.out.println("premi 4 per calcolare l'altezza");
+            System.out.println("premi 5 per calcolare il perimetro");
+            System.out.println("premi 6 per calcolare l'area");
+
+            switch (key) {
+                case value:
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        }while(scelta!=0);
+    }
+}
