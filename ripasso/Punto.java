651c229fab50c92b84c16646db61e62790ae2f77
Your NamePando13 <meme04pando@gmail.com>
2023-05-04 19:23:02 +0200

    ripasso

---
+++ ripasso/Punto.java
@@ -0,0 +1,11 @@
+package ripasso;
+import java.util.*;
+
+public class Punto {
+    int numero;
+    String stringa;
+    public Punto(int numero, String stringa){
+        this.numero=numero;
+        this.stringa=stringa;
+    }
+}
---
+++ ripasso/Test.java
@@ -0,0 +1,38 @@
+package ripasso;
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+		Vector<Punto> poligono=new Vector<Punto>(1,1);
+        Punto p=null;
+        String vuota;
+        int scelta;
+        do {
+            System.out.println("input:");
+            scelta=input.nextInt();
+            vuota=input.nextLine();
+            switch(scelta){
+                case 0:
+                    break;
+                    
+                case 1:
+                    System.out.println("intero");
+                    numero=input.nextInt();
+                    vuota=input.nextLine();
+                    p = new Punto(numero, stringa)
+                    break;
+
+                case 2:
+                    for (i=0;i<poligono.size();i++) {
+                        p=poligono.get(i);
+                        
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        } while (scelta!=0);
+    }
+}
